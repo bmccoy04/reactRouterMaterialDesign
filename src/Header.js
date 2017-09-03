22e2484d732d7188c8d65b7cc3fa3eadbb667dc1
bmccoy04 <bryanmccoy04@gmail.com>
2017-09-03 18:10:14 -0500

    material ui addeda

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { AppBar } from 'material/lib/app-bar'
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/second'>Second</Link></li>
-      </ul>
-    </nav>
-  </header>
+  <AppBar title="Title"
+  />
+  // <header>
+  //   <nav>
+  //     <ul>
+  //       <li><Link to='/'>Home</Link></li>
+  //       <li><Link to='/second'>Second</Link></li>
+  //     </ul>
+  //   </nav>
+  // </header>
 )
 
 export default Header
