4ad3fd054ac52994ec7c98d639156f064bf43a31
bmccoy04 <bryanmccoy04@gmail.com>
2017-09-05 20:38:09 -0500

    added drawer w/ links

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,22 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 // The Header creates links that can be used to navigate
 // between routes.
-const Header = () => (
-  <header>
-  <AppBar title="Title" />
-  </header>
-  
-  // <header>
-  //   <nav>
-  //     <ul>
-  //       <li><Link to='/'>Home</Link></li>
-  //       <li><Link to='/second'>Second</Link></li>
-  //     </ul>
-  //   </nav>
-  // </header>
-)
+export default class Header extends React.Component {
 
-export default Header
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+
+  handleToggle = () => this.setState({open: !this.state.open});
+  
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+      <header>
+        <AppBar title="Title" 
+        onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({ open })}
+        >
+        <AppBar />
+          <MenuItem onClick={this.handleClose} containerElement={<Link to="/" />} >Home</MenuItem>
+          <MenuItem onClick={this.handleClose} containerElement={<Link to="/second" />} >Second</MenuItem>
+        </Drawer>
+      </header>
+    )
+
+    // <header>
+    //   <nav>
+    //     <ul>
+    //       <li><Link to='/'>Home</Link></li>
+    //       <li><Link to='/second'>Second</Link></li>
+    //     </ul>
+    //   </nav>
+    // </header>
+  }
+}
